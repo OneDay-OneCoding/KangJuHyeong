39dcda744f12348f65c04c28ca9f1847f37e1854
kakaopanda <kakao_panda@naver.com>
2023-06-10 23:22:47 +0900

    20230610

---
+++ PROG/Level_0/수열과 구간 쿼리 2.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = new int[queries.length];
+        for(int i=0; i<queries.length; i++){
+            int min = -1;
+            for(int j=queries[i][0]; j<=queries[i][1]; j++){
+                if(arr[j] > queries[i][2]){
+                    if(min != -1){
+                        min = Math.min(min,arr[j]);
+                    }
+                    else{
+                        min = arr[j];    
+                    }
+                    answer[i] = min;
+                }
+                else{
+                    continue;
+                }
+            }
+            answer[i] = min;
+        }
+        return answer;
+    }
+}
