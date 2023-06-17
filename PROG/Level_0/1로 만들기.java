538163dfa54a08516bf3ae115fb2f4e2577667bb
kakaopanda <kakao_panda@naver.com>
2023-06-17 11:54:47 +0900

    20230617

---
+++ PROG/Level_0/1로 만들기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        for(int i=0; i<num_list.length; i++){
+            while(true){
+                if(num_list[i]!=1){
+                    num_list[i] /= 2;
+                    answer++;
+                }
+                else{
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}
