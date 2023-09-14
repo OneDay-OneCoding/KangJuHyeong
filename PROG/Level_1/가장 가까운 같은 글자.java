76043d6dc23ac2f688e59f52e58bf1c8658240f1
kakaopanda <kakao_panda@naver.com>
2023-09-14 11:06:27 +0900

    20230914

---
+++ PROG/Level_1/가장 가까운 같은 글자.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] solution(String s) {
+        int[] answer = new int[s.length()];
+        
+        for(int i=0; i<s.length(); i++){
+            char target = s.charAt(i);
+            
+            for(int j=i-1; j>=0; j--){
+                char compare = s.charAt(j);
+
+                if(compare == target){
+                    answer[i] = i-j;
+                    break;
+                }
+            }
+            if(answer[i] == 0){
+                answer[i] = -1;
+            }
+        }
+        return answer;
+    }
+}
