d7a51d8eb57804fbdd4f9b9821c6d43bc08b6de1
kakaopanda <kakao_panda@naver.com>
2023-11-14 13:29:23 +0900

    20231114

---
+++ PROG/Level_2/최댓값과 최솟값.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        String[] str = s.split(" ");
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        
+        for(int i=0; i<str.length; i++){
+            min = Math.min(min, Integer.parseInt(str[i]));
+            max = Math.max(max, Integer.parseInt(str[i]));
+        }
+        
+        answer += min + " " + max;
+        return answer;
+    }
+}
